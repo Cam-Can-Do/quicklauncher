f8a3b588ec7b3b53bd9c78384a6ae7957e0738a0
Cameron Candau <camcandau@gmail.com>
2023-08-18 11:53:48 -0700

    Command execution changes

--- launcher.cpp
+++ launcher.cpp
@@ -3,11 +3,14 @@
 
 int main(){
     std::string line;
-    std::ifstream input("actions.txt");
+    std::string path = (std::string(std::getenv("HOME")) + "/.config/quicklauncher/actions.txt");
+    std::ifstream input(path);
     if (input.is_open()){
         while(std::getline(input, line)){
-            line += "&";    // "&" says to run in background
+            line = "nohup " + line + " &";    // "&" says to run in background
+            std::cout << "Executing: " << line << std::endl;
             system(line.c_str());
         }
         input.close();
+    }
 }
